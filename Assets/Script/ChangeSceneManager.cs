3c284562cfc3126e353b8c881bbae67cbb748680
syuyucheng29 <syuyucheng29@gmail.com>
2021-12-17 00:17:09 +0800

    SceneManager modifeid

--- Assets/Script/ChangeSceneManager.cs
+++ Assets/Script/ChangeSceneManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeSceneManager : MonoBehaviour
 {
     public static ChangeSceneManager instance=null;
+    private GameObject menuUI;
+    private GameObject gameUI;
+
     void Awake()
     {
         if (instance != null)
@@ -18,11 +22,22 @@ public class ChangeSceneManager : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
         instance = this;
-    }
 
-    void Update()
-    {
+        menuUI = transform.FindAnyChild<Transform>("MenuUI").gameObject;
+        menuUI.SetActive(true);
+        gameUI = transform.FindAnyChild<Transform>("GameUI").gameObject;
+        gameUI.SetActive(true);
     }
-
     public static void ChangeScene(string sceneName) => SceneManager.LoadScene(sceneName);
+    public void OnEnable()=>SceneManager.sceneLoaded += OnSceneLoaded;
+    public void OnDisable() => SceneManager.sceneLoaded -= OnSceneLoaded;
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "Menu")
+            menuUI.SetActive(false);
+        else
+            menuUI.SetActive(true);
+
+        gameUI.SetActive(!menuUI.activeSelf);
+    }
 }
